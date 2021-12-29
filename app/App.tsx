bb639d43223e9c770fc071871ced7e18f6087706
Aymeric Chauvin <contact@halftheopposite.dev>
2021-12-29 17:09:40 +0100

    Clean Home screen

--- app/App.tsx
+++ app/App.tsx
@@ -8,7 +8,7 @@ const Stack = createNativeStackNavigator();
 export function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,52 +1,14 @@
 import React from "react";
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
-import { useUsers } from "../hooks";
+import { StyleSheet, Text, View } from "react-native";
 
 export function HomeScreen() {
-  const { users, loadUsers } = useUsers();
-
-  React.useEffect(() => {
-    loadUsers();
-  }, []);
-
   return (
     <View style={styles.container}>
-      <ScrollView>
-        {users.map((user, index) => (
-          <View
-            key={index}
-            style={[
-              styles.user,
-              index < users.length - 1 ? { marginBottom: 16 } : {},
-            ]}
-          >
-            <Image
-              source={{ uri: user.pictures[0] }}
-              style={{
-                width: 128,
-                height: 128,
-                resizeMode: "cover",
-              }}
-            />
-            <Text>Name: {user.name}</Text>
-            <Text>Age: {user.age}</Text>
-            <Text>Distance: {user.distance}km</Text>
-            <Text>Bio: {user.bio}</Text>
-          </View>
-        ))}
-      </ScrollView>
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "red",
-  },
-  user: {
-    display: "flex",
-    borderColor: "black",
-    borderWidth: 2,
-  },
+  container: { flex: 1, alignItems: "center", justifyContent: "center" },
 });
